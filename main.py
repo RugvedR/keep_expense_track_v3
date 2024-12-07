6360e10e8303da761f383f5f80fdfb5541e3cb5d
RugvedR <rugvedr9@gmail.com>
2024-12-07 18:55:36 +0530

    Read from txt, insights support added

--- main.py
+++ main.py
@@ -3,82 +3,17 @@ from expenses_utils import update_expenses, update_monthly_expenses
 # Define file paths
 daily_expenses_file = "expenses.xlsx"
 monthly_expenses_file = "monthly_expenses.xlsx"
-
-# Note for updating daily expenses
-note = """
-24.11.24
-110/- lunch
-160/- dinner
-
-25.11.24
-66/- lunch
-500/- country delight milk
-234/- peanut butter,etc
-25/- neembu cucumber 
-
-26.11.24
-43/- Paratha
-83/- dinner 
-
-27.11.24
-100/- clotrimazole cream
-49/- paratha
-75/- dinner
-115/- bottle
-55/- stock
-
-28.11.24
-166/- lunch
-2222/- shoe, bine cover, trackpant
-75/- dinner
-
-29.11.24
-211/- dalbati plus zomato gold
-155/- dinner
-12/- misc
-
-30.11.24
-25/- pohe
-473/- healthy things grocery
-500/- coconut
-80/- paneer and veggie
-
-1.12.24
-300/- petrol
-Piyush home lunch, badmin 🔵
-30/- pinapple juice from piyush, gore 🔴🔴
-166/- dinner
-790/- harmosa eats
-450/- maid
-6250/- rent
-300/- misc
-
-2.12.24
-199/- Netflix 
-151/- rice, aata
-150/- dinner
-
-3.12.24
-70/- paratha
-93/- dinner
-
-4.12.24
-111/- lunch
-141/- dinner
-
-5.12.24
-Dinner to somesh 🔵
-
-6.12.24
-214/- electricity bill 
-38/- cab for party
-34/- misc
-
-7.12.24
-77/- lunch
-"""
+notes_file = "note.txt"
 
 def main():
+    # Read notes from the file
+    try:
+        with open(notes_file, 'r') as file:
+            note = file.read().strip()
+    except FileNotFoundError:
+        print(f"Error: The file '{notes_file}' does not exist.")
+        return
+    
     # Update daily expenses
     new_entries = update_expenses(note, daily_expenses_file)
     if new_entries > 0:
